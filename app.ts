90ce26c97ed68cf6d6606a194aad5512db293a08
Smilex11 <106424141+Smilex11@users.noreply.github.com>
2022-08-28 16:52:47 +0800

    Create app.ts

---
+++ app.ts
@@ -0,0 +1,20 @@
+import * as Drash from "https://deno.land/x/drash@<VERSION>/mod.ts";
+
+class HomeResource extends Drash.Resource {
+  paths = ["/"];
+
+  public GET(_request: Drash.Request, response: Drash.Response) {
+    response.text("Hello world!");
+  }
+}
+
+const server = new Drash.Server({
+  resources: [
+    HomeResource,
+  ],
+  protocol: "http",
+  hostname: "localhost",
+  port: 3000,
+});
+
+server.run();
